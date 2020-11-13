33f5dbb213ad3a989c92043bc2fd5abe7fef6ed7
Steven Bong <stevenseal99@yahoo.com>
2020-11-13 13:50:08 -0800

    Add products layout page using Bulma and Material UI framework

--- src/pages/User Dashboard/Product/Product.js
+++ src/pages/User Dashboard/Product/Product.js
@@ -1,10 +1,50 @@
 import React from "react";
+import ProductResult from "./ProductResult";
+import "./product.css";
 
 function Product() {
-    return(
-        <div>
-            <h1>This is the product page</h1>
-        </div>
+    return (
+        <>
+            <div className="columns productStyle is-justify-content-center">
+                <div className="column">
+                    <div className="field">
+                        <div className="control has-icons-left">
+                            <div className="select">
+                                <select>
+                                    <option selected>Filter</option>
+                                    <option>Put here</option>
+                                    <option>Put here</option>
+                                </select>
+                                <div className="icon is-small is-left">
+                                    <i className="fas fa-info-circle"></i>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="column">
+
+                </div>
+
+                <div className="column">
+
+                </div>
+
+                <div className="column">
+                    <div className="control has-icons-left has-icons-right">
+                        <input className="input is-rounded" type="text" placeholder="Rounded input" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-search"></i>
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+            <ProductResult />
+        </>
     )
 }
 
