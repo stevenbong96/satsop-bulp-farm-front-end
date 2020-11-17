8ba3aae79ad76ecae9f01ed330d92ebda9e2ae24
Brandon Phillips <brandon.phillips976@gmail.com>
2020-11-17 11:01:25 -0800

    user now has full crud functionality of faq update page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UserDashboard from './pages/User Dashboard/UserDashboard';
 // import AboutUpdate from "./pages/AboutUpdate"
 import BasicInfoUpdate from "./pages/BasicInfoUpdate"
 import FAQUpdate from './pages/FAQUpdate'
+import HomeUpdate from './pages/HomeUpdate'
 
 function App() {
   return (
---
+++ src/pages/HomeUpdate.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+export default function HomeUpdate() {
+    const [welcomeText, setWelcomeText] = useState()
+
+    const [briefAboutText, setBriefAboutText] = useState()
+    const [aboutText, setAboutText] = useState({})
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
