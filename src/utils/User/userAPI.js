935632d27910ddca89d0a41c7f4587b1a69fe7e4
Steven Bong <stevenseal99@yahoo.com>
2020-11-16 16:27:16 -0800

    Add search functionality to products page

---
+++ src/utils/User/userAPI.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+export default {
+  getAllProducts: function() {
+    const BASEURL = "https://calm-brook-21723.herokuapp.com/api/products";
+    return axios.get(BASEURL);
+  },
+  getProductsName: function(query) {
+    const BASEURL = `https://calm-brook-21723.herokuapp.com/api/product/${query}`;
+    return axios.get(BASEURL);
+  }
+}; 
