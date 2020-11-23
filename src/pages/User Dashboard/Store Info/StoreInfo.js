e3b4459a3fd8e8f4e61550b3bc4267ebbb763313
Steven Bong <stevenseal99@yahoo.com>
2020-11-23 09:55:29 -0800

    Fixed the store info render

--- src/pages/User Dashboard/Store Info/StoreInfo.js
+++ src/pages/User Dashboard/Store Info/StoreInfo.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React , {useState, useEffect}from "react";
 // import MapView from "./mapView";
 import "./storeInfo.css";
 // import 'leaflet/dist/leaflet.css';
 // import {Link} from "react-router-dom";
+import API from "../../../utils/User/userAPI";
 
 function StoreInfo(props) {
+    const [currentStoreInfo, setCurrentStoreInfo] = useState([]);
+
+    useEffect(() => {
+        loadStoreInfo();
+    }, [])
+
+    function loadStoreInfo(){
+        API.getStoreInfo()
+        .then(res => {
+            // console.log(res.data);
+            setCurrentStoreInfo(res.data)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+
+    // {currentStoreInfo.map(storeObj => {
+    //     console.log(storeObj)
+    //     return <phoneNumber={storeObj.phoneNumber} email={storeObj.companyEmail} hours={storeObj.hours}/>
+    // })}
+
     return (
         <div className="storeStyle">
             <h2 className="title is-2">Store Info</h2>
@@ -12,17 +36,18 @@ function StoreInfo(props) {
             <h6 className="subtitle is-6 storeText has-text-white">{props.storeText}</h6>
             <div className="columns is-justify-content-center storeText">
                 <div className="column is-12">
-                    <h6 className="subtitle is-6 has-text-white">Phone Number: 360-482-5566</h6>
+                    <h6 className="subtitle is-6 has-text-white">Phone Number: {currentStoreInfo.map(phoneObj => phoneObj.phoneNumber)}</h6>
                 </div>
             </div>
             <div className="columns is-justify-content-center storeText">
                 <div className="column is-12">
-                    <h6 className="subtitle is-6 has-text-white">Email: info@satsopbulbfarm.com {props.email}</h6>
+                    <h6 className="subtitle is-6 has-text-white">Email: {currentStoreInfo.map(emailObj => emailObj.companyEmail)}</h6>
                 </div>
             </div>
             <div className="columns is-justify-content-center storeText">
                 <div className="column is-12">
-                    <h6 className="subtitle is-6 has-text-white">Store Hours: Monday-Sunday 10AM - 5PM</h6>
+                    <h6 className="subtitle is-6 has-text-white">Store Hours: Monday-Friday 10AM - 5PM</h6>
+                    {/* <p>{currentStoreInfo.map(hoursObj => hoursObj.map(hoursObj2 => console.log(hoursObj2)))}</p> */}
                 </div>
             </div>
         </div>
