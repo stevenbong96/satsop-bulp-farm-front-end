576152144400fc6298b87df48ae4ca4d4def3eaa
Brandon Phillips <brandon.phillips976@gmail.com>
2020-11-22 16:47:54 -0800

    added axios request to delete product

--- src/utils/API.js
+++ src/utils/API.js
@@ -23,7 +23,7 @@ export default {
         return axios.put('https://calm-brook-21723.herokuapp.com/api/update/product/' + id, productObj)
     },
     deleteProduct: function (id) {
-        // return axios.delete('https://calm-brook-21723.herokuapp.com/api/product/' + id)
+        return axios.delete('https://calm-brook-21723.herokuapp.com/api/product/' + id)
     },
     getFAQ: function () {
         return axios.get('https://calm-brook-21723.herokuapp.com/api/faqText')
