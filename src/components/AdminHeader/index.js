1f7b86ff8722724a5e9778d981f478943366564c
Brandon Phillips <brandon.phillips976@gmail.com>
2020-11-30 12:28:53 -0800

    added logout button to admin dashboard

--- src/components/AdminHeader/index.js
+++ src/components/AdminHeader/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
- 
 import './index.css'
 
 export default function AdminHeader() {
+    const adminLogout = () => {
+        localStorage.removeItem('token')
+        window.location.href='/'
+    }
     
     return (
         <div className="adminHeader columns">
-            <h1 className="header ">Admin Dashboard</h1>
+            <h1 className="header admin-header-text">Admin Dashboard</h1>
+            <button className='logout-btn' onClick={adminLogout}>Logout</button>
         </div>
     )
 }
